2af0a553510b67129c0d4e97ab7857d610d19df1
ksr-ccb <harang4282@gmail.com>
2025-02-26 14:23:23 +0900

    Calculator 클래스의 연산 결과를 저장하고 있는 컬렉션 필드생성
    Calculator 클래스에 저장된 연산 결과들 중 가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드를 구현한 후 App 클래스의 main 메서드에 삭제 메서드가 활용될 수 있도록 수정

--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -1,11 +1,17 @@
 package level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
+    private int[] values = new int[2];
+    private int result= 0;
+    private char operator;
+
+    private List<Integer> savedResults= new ArrayList<>();
 
     //계산 함수
     int operation(int[] values, char operator) {
-        int result= 0;
-
         if((values.length !=2)){
             System.out.println("계산할 정수가 올바르게 입력되지 않았어요");
         }
@@ -54,20 +60,28 @@ public class Calculator {
         return result;
     }
 
-    void operationException(int[] values, char operator) {
+    void operationException() {
         try{
             switch (operator){
                 case '+':
+                    result = (values[0] + values[1]);
                     System.out.println("더하기 계산 결과는 " + (values[0] + values[1]) + "입니다.");
+                    savedResults.add(result);
                     break;
                 case '-':
+                    result = (values[0] - values[1]);
                     System.out.println("빼기 계산 결과는 " + (values[0] - values[1]) + "입니다.");
+                    savedResults.add(result);
                     break;
                 case '*':
+                    result = (values[0] * values[1]);
                     System.out.println("곱하기 계산 결과는 " + (values[0] * values[1]) + "입니다.");
+                    savedResults.add(result);
                     break;
                 case '/':
+                    result = (values[0] / values[1]);
                     System.out.println("나누기 계산 결과는 " + (values[0] / values[1]) + "입니다.");
+                    savedResults.add(result);
                     break;
             }
         }catch (Exception e){
@@ -76,7 +90,7 @@ public class Calculator {
     }
 
      boolean checkContinue(String str) {
-        if(str.equals("exit")){
+        if(str.equalsIgnoreCase("exit")){
             System.out.println("계산기를 종료합니다.");
             return false;
         }
@@ -85,5 +99,35 @@ public class Calculator {
             System.out.println("계산을 다시 시작합니다.");
             return true;
         }
+    }
+
+    int getResult() {
+        return result;
+    }
+
+    void setResult(int result) {
+        this.result = result;
+    }
+
+    int[] getValues() {
+        return values;
+    }
+
+    void setValues(int value, int index) {
+        this.values[index] = value;
+    }
+
+    char getOperator() {
+        return operator;
+    }
+    void setOperator(char operator) {
+        this.operator = operator;
+    }
+
+    List<Integer> getSavedResults() {
+        return savedResults;
+    }
+    void deleteSavedResult(){
+        savedResults.remove(0);
     }
 }
--- src/level2/Main.java
+++ src/level2/Main.java
@@ -12,17 +12,20 @@ public class Main {
         int[] values = new int[2];
         boolean exitFlag = true;
         //int result = 0;
+        String finishStr;
 
         while(exitFlag){
             //계산기에 들어갈 수 입력 받기
             for(int i = 0; i < 2; i++){
-                System.out.println("연산을 진행할 "+ (i+1) +"번째 정수를 입력해주세요 : ");
+                System.out.print("연산을 진행할 "+ (i+1) +"번째 정수를 입력해주세요 : ");
                 try{
                     values[i] = sc.nextInt();
                     if(values[i] < 0){
                         System.out.println("0 이상의 값만 입력이 가능해요.");
                         sc.nextLine();
                         i--;
+                    }else{
+                        calculator.setValues(values[i], i);
                     }
                 }catch (InputMismatchException e) {
                     System.out.println("정수 값을 입력해주세요!");
@@ -32,45 +35,65 @@ public class Main {
             }
 
             // 입력 유효성 검사
-//
-            //////////// 입력 유효성 검사/////////// boolean chkValidation = true;
-            /// //            char operator = 0;
-            /// //            while(chkValidation){
-            /// //                System.out.println(values[0] + "와" + values[1] + " 사이 계산할 사칙연산 기호를 정해주세요: ");
-            /// //                operator = sc.next().charAt(0);
-            /// //                sc.nextLine();
-            /// //
-            /// //                if(operator == '+'){
-            /// //                    chkValidation = false;
-            /// //                }else if (operator == '-'){
-            /// //                    chkValidation = false;
-            /// //                }else if (operator == '*'){
-            /// //                    chkValidation = false;
-            /// //                }else if (operator == '/'){
-            /// //                    if(values[1] == 0){
-            /// //                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없어요");
-            /// //                    }
-            /// //                    else{
-            /// //                        chkValidation = false;
-            /// //                    }
-            /// //                }else{
-            /// //                    System.out.println("사칙 연산 기호는 +,-,*,/만 사용가능해요.");
-            /// //                }
-            /// //            }
-
-
+            //////////// 입력 유효성 검사///////////
+            // //            boolean chkValidation = true;
+            // //            char operator = 0;
+            // //            while(chkValidation){
+            // //                System.out.println(values[0] + "와" + values[1] + " 사이 계산할 사칙연산 기호를 정해주세요: ");
+            // //                operator = sc.next().charAt(0);
+            // //                sc.nextLine();
+            // //
+            // //                if(operator == '+'){
+            // //                    chkValidation = false;
+            // //                }else if (operator == '-'){
+            // //                    chkValidation = false;
+            // //                }else if (operator == '*'){
+            // //                    chkValidation = false;
+            // //                }else if (operator == '/'){
+            // //                    if(values[1] == 0){
+            // //                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없어요");
+            // //                    }
+            // //                    else{
+            // //                        chkValidation = false;
+            // //                    }
+            // //                }else{
+            // //                    System.out.println("사칙 연산 기호는 +,-,*,/만 사용가능해요.");
+            // //                }
+            // //            }
             /// /////////입력 끝 ///////////////
             //exception
-            System.out.println(values[0] + "와" + values[1] + " 사이 계산할 사칙연산 기호를 정해주세요: ");
+            System.out.print(values[0] + "와" + values[1] + " 사이 계산할 사칙연산 기호를 정해주세요: ");
             char operator = sc.next().charAt(0);
             sc.nextLine();
-            calculator.operationException(values, operator);
+            if(operator == '+' || operator == '-'||operator == '*'||operator == '/'){
+                calculator.setOperator(operator);
+            }else{
+                System.out.println(operator + "은 유효한 연산 기호가 아니에요 ");
+            }
 
-            //System.out.println("계산 결과 값은 : " + calculator.operationChkValidation(values, operator) + " 입니다");
+            calculator.operationException();
+
+            System.out.println("계산 결과 값은 : " + calculator.getResult() + " 입니다");
             /// ///////////////계산기는 끝/////////////////
 
-            System.out.print("계산을 종료하시려면 'exit' 를 입력해주세요 : ");
-            exitFlag = calculator.checkContinue(sc.nextLine());
+            System.out.println("//////////////////////////////////////");
+            System.out.println("계산기를 계속 진행하시겠습니까?");
+            System.out.print("계산을 종료하시려면 'exit' , 누적 계산 결과를 출력하시려면 'total' 을 입력하세요 : ");
+            finishStr = sc.nextLine();
+            if(finishStr.equalsIgnoreCase("total")){
+                for (Integer i : calculator.getSavedResults()) {
+                    System.out.print(i + ", ");
+                }
+                System.out.println("처음 누적 결과값을 삭제할 수 있어요.");
+                System.out.print("처음 누적 결과값을 삭제하려면 'delete' , 계산을 종료하시려면 'exit' 를 입력하세요 : ");
+
+                finishStr = sc.nextLine();
+                if(finishStr.equalsIgnoreCase("delete")){
+                    calculator.deleteSavedResult();
+                }
+            }
+            exitFlag = calculator.checkContinue(finishStr);
+            System.out.println("//////////////////////////////////////");
 
         }
 
